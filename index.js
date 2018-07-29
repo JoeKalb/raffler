9413d8574ec36c757f46b19c4e54d1f95226e174
Joe.Kalb <kalb.joseph@gmail.com>
2018-07-29 16:18:06 -0700

    added node server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express();
+const http = require('http').Server(app);
+const path = require('path');
 
+const port = process.env.PORT || 8000;
+
+app.use('/node_modules/bootstrap/', express.static(path.join(__dirname + '/node_modules/bootstrap/')));
+app.use('/', express.static(path.join(__dirname + '/')));
 app.get('/', (req, res) => {
-  res.send('hiya!')
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+http.listen(port, () => {
+  console.log('Example app listening on port ' + port);
 });
