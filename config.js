71e7fdf211ec52d80647086351caad23b1074f65
Joe.Kalb <kalb.joseph@gmail.com>
2018-12-22 21:20:28 -0800

    added socket io

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports.CONFIG =  {
+  CLIENT_ID: "aomhov08QJpP4x8tsnocdO1YAJYzY55GJplEX6yP",
+  CLIENT_SECRET: "gzfOz4xk3NnuDsQTCUAzLU6Mj6Ly8PhLYpBp767x",
+  REDIRECT_URI: "http://localhost:8000/"
+}
--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express();
 const http = require('http').Server(app);
 const path = require('path');
+const oi = require('socket.io')(http);
+const ClientOAuth2 = require('client-oauth2');
+
+let CONFIG;
+if (process.env.CLIENT_ID !== undefined){
+  CONFIG = {
+	  CLIENT_SECRET: process.env.CLIENT_SECRET,
+	  CLIENT_ID: process.env.CLIENT_ID,
+    REDIRECT_URI: process.env.REDIRECT_URI
+  }
+} 
+else {
+  const localConfig = require('./config');
+  CONFIG = localConfig.CONFIG;
+};
 
 const port = process.env.PORT || 8000;
 
@@ -10,6 +25,24 @@ app.use('/', express.static(path.join(__dirname + '/')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+let streamlabsAuth;
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({"info":"PONG"});
+})
+
+app.get('/login', (req, res) => {
+  streamlabsAuth = new ClientOAuth2({
+    clientId: CONFIG.CLIENT_ID,
+    clientSecret: CONFIG.CLIENT_SECRET,
+    redirectUri: CONFIG.REDIRECT_URI,
+    authorizationUri: 'https://streamlabs.com/api/v1.0/authorize',
+    accessTokenUri: 'https://streamlabs.com/api/v1.0/token',
+    scopes: ['socket.token']
+  })
+  res.redirect(streamlabsAuth.redirectUri)
+})
 
 http.listen(port, () => {
   console.log('Example app listening on port ' + port);
